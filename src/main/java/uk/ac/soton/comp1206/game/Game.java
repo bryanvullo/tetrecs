62978c4756f733287514513386ce9df4f446b98b
bryanvullo <bryanvullo04@gmail.com>
2023-03-18 22:07:58 +0000

    initial commit

---
+++ src/main/java/uk/ac/soton/comp1206/game/Game.java
@@ -0,0 +1,174 @@
+package uk.ac.soton.comp1206.game;
+
+import java.util.HashSet;
+import java.util.Random;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import uk.ac.soton.comp1206.component.GameBlock;
+import uk.ac.soton.comp1206.component.GameBlockCoordinate;
+
+/**
+ * The Game class handles the main logic, state and properties of the TetrECS game. Methods to manipulate the game state
+ * and to handle actions made by the player should take place inside this class.
+ */
+public class Game {
+    
+    private static final Logger logger = LogManager.getLogger(Game.class);
+    
+    /**
+     * Number of rows
+     */
+    protected final int rows;
+    
+    /**
+     * Number of columns
+     */
+    protected final int cols;
+    
+    /**
+     * The grid model linked to the game
+     */
+    protected final Grid grid;
+    
+    /**
+     * random field variable used to generate pieces
+     */
+    private final Random random = new Random();
+    
+    /**
+     * field variable which stores the current piece model
+     */
+    public GamePiece currentPiece;
+    
+    /**
+     * Create a new game with the specified rows and columns. Creates a corresponding grid model.
+     * @param cols number of columns
+     * @param rows number of rows
+     */
+    public Game(int cols, int rows) {
+        this.cols = cols;
+        this.rows = rows;
+        
+        //Create a new grid model to represent the game state
+        this.grid = new Grid(cols,rows);
+    }
+    
+    /**
+     * Start the game
+     */
+    public void start() {
+        logger.info("Starting game");
+        initialiseGame();
+    }
+    
+    /**
+     * Initialise a new game and set up anything that needs to be done at the start
+     */
+    public void initialiseGame() {
+        logger.info("Initialising game");
+        currentPiece = spawnPiece();
+    }
+    
+    /**
+     * Handle what should happen when a particular block is clicked
+     * @param gameBlock the block that was clicked
+     */
+    public void blockClicked(GameBlock gameBlock) {
+        //Get the position of this block
+        int x = gameBlock.getX();
+        int y = gameBlock.getY();
+        
+        if (!grid.canPlayPiece(currentPiece, x, y)) return;
+        
+        grid.playPiece(currentPiece, x, y);
+        nextPiece();
+        
+        //Get the new value for this block
+        int previousValue = grid.get(x,y);
+        int newValue = previousValue + 1;
+        if (newValue  > GamePiece.PIECES) {
+            newValue = 0;
+        }
+        
+        //Update the grid with the new value
+        grid.set(x,y,newValue);
+    }
+    
+    /**
+     * Get the grid model inside this game representing the game state of the board
+     * @return game grid model
+     */
+    public Grid getGrid() {
+        return grid;
+    }
+    
+    /**
+     * Get the number of columns in this game
+     * @return number of columns
+     */
+    public int getCols() {
+        return cols;
+    }
+    
+    /**
+     * Get the number of rows in this game
+     * @return number of rows
+     */
+    public int getRows() {
+        return rows;
+    }
+    
+    /**
+     * this method generates a random number and returns a new random GamePiece
+     * @return random GamePiece
+     */
+    public GamePiece spawnPiece() {
+        logger.info("generating a new piece");
+        var piece = random.nextInt(15);
+        return GamePiece.createPiece(piece);
+    }
+    
+    /**
+     * replaces the current GamePiece with a new GamePiece
+     */
+    public void nextPiece() {
+        currentPiece = spawnPiece();
+    }
+    
+    /**
+     * logic to handle what happens after a GamePiece has been placed
+     * Clearing lines
+     */
+    public void afterPiece() {
+        logger.info("checking if any rows or columns are full");
+        var blocksToClear = new HashSet<GameBlockCoordinate>();
+        //checking rows
+        for (int row = 0; row < rows; row++) {
+            var counter = 0;
+            for (int col = 0; col < cols; col++) {
+                if (grid.get(col,row) > 0) counter++; //count how many occupied in this row
+            }
+            if (counter == cols) { //if row is full, add row to the clear list
+                for (int col = 0; col < cols; col++) {
+                    blocksToClear.add(new GameBlockCoordinate(col, row));
+                }
+            }
+        }
+        //checking columns
+        for (int col = 0; col < cols; col++) {
+            var counter = 0;
+            for (int row = 0; row < cols; row++) {
+                if (grid.get(col,row) > 0) counter++; //count how many occupied in this row
+            }
+            if (counter == rows) { //if row is full, add row to the clear list
+                for (int row = 0; row < rows; row++) {
+                    blocksToClear.add(new GameBlockCoordinate(col, row));
+                }
+            }
+        }
+        //clear all blocks in the clear list
+        for (var block : blocksToClear) {
+            grid.set(block.getX(), block.getY(), 0);
+        }
+    }
+}
---
+++ src/main/java/uk/ac/soton/comp1206/game/Grid.java
@@ -0,0 +1,170 @@
+package uk.ac.soton.comp1206.game;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * The Grid is a model which holds the state of a game board. It is made up of a set of Integer
+ * values arranged in a 2D arrow, with rows and columns.
+ * <p>
+ * Each value inside the Grid is an IntegerProperty can be bound to enable modification and display
+ * of the contents of the grid.
+ * <p>
+ * The Grid contains functions related to modifying the model, for example, placing a piece inside
+ * the grid.
+ * <p>
+ * The Grid should be linked to a GameBoard for it's display.
+ */
+public class Grid {
+    
+    private static final Logger logger = LogManager.getLogger(Grid.class);
+    
+    /**
+     * The number of columns in this grid
+     */
+    private final int cols;
+    
+    /**
+     * The number of rows in this grid
+     */
+    private final int rows;
+    
+    /**
+     * The grid is a 2D arrow with rows and columns of SimpleIntegerProperties.
+     */
+    private final SimpleIntegerProperty[][] grid;
+    
+    /**
+     * Create a new Grid with the specified number of columns and rows and initialise them
+     *
+     * @param cols number of columns
+     * @param rows number of rows
+     */
+    public Grid(int cols, int rows) {
+        this.cols = cols;
+        this.rows = rows;
+        
+        //Create the grid itself
+        grid = new SimpleIntegerProperty[cols][rows];
+        
+        //Add a SimpleIntegerProperty to every block in the grid
+        for (var y = 0; y < rows; y++) {
+            for (var x = 0; x < cols; x++) {
+                grid[x][y] = new SimpleIntegerProperty(0);
+            }
+        }
+    }
+    
+    /**
+     * Get the Integer property contained inside the grid at a given row and column index. Can be used
+     * for binding.
+     *
+     * @param x column
+     * @param y row
+     * @return the IntegerProperty at the given x and y in this grid
+     */
+    public IntegerProperty getGridProperty(int x, int y) {
+        return grid[x][y];
+    }
+    
+    /**
+     * Update the value at the given x and y index within the grid
+     *
+     * @param x     column
+     * @param y     row
+     * @param value the new value
+     */
+    public void set(int x, int y, int value) {
+        grid[x][y].set(value);
+    }
+    
+    /**
+     * Get the value represented at the given x and y index within the grid
+     *
+     * @param x column
+     * @param y row
+     * @return the value
+     */
+    public int get(int x, int y) {
+        try {
+            //Get the value held in the property at the x and y index provided
+            return grid[x][y].get();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //No such index
+            return -1;
+        }
+    }
+    
+    /**
+     * Get the number of columns in this game
+     *
+     * @return number of columns
+     */
+    public int getCols() {
+        return cols;
+    }
+    
+    /**
+     * Get the number of rows in this game
+     *
+     * @return number of rows
+     */
+    public int getRows() {
+        return rows;
+    }
+    
+    /**
+     * Returns true if the piece can be played at a certain coordinate
+     *
+     * @param piece the piece to play
+     * @param x     the x coordinate of the clicked block
+     * @param y     the y coordinate of the clicked block
+     * @return if the piece can be played
+     */
+    public Boolean canPlayPiece(GamePiece piece, int x, int y) {
+        logger.info("checking if piece can be played");
+        var blocks = piece.getBlocks();
+        int pieceX = 0;
+        for (int i = y - 1; (i < y + 2 & i < rows); i++) {
+            int pieceY = 0;
+            for (int j = x - 1; (j < x + 2 & j < cols); j++) {
+                if (blocks[pieceX][pieceY] > 0 & grid[x][y].get() > 0) {
+                    logger.info("piece cannot be placed");
+                    return false;
+                }
+                pieceY++;
+            }
+            pieceX++;
+        }
+        logger.info("piece can be placed");
+        return true;
+    }
+    
+    /**
+     * plays the current piece onto the board
+     *
+     * @param piece the piece to play
+     * @param x     the x coordinate of the clicked block
+     * @param y     the y coordinate of the clicked block
+     */
+    public void playPiece(GamePiece piece, int x, int y) {
+        var blocks = piece.getBlocks();
+        int pieceX = 0;
+        //iterates over the size of the piece or until the edge of the grid
+        for (int i = y - 1; (i < y + 2 & i < rows); i++) {
+            int pieceY = 0;
+            for (int j = x - 1; (j < x + 2 & j < cols); j++) {
+                if (blocks[pieceX][pieceY] > 0) {
+                    grid[x][y].set(blocks[pieceX][pieceY]);
+                }
+                pieceY++;
+            }
+            pieceX++;
+        }
+        logger.info("played a piece");
+    }
+    
+    
+}
