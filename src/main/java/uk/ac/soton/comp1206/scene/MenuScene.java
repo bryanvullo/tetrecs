73f0bf453db0f6ba3a762f028b2228af2b144ac5
bryanvullo <bryanvullo04@gmail.com>
2023-03-28 00:13:48 +0100

    on initiation the class displays a fade animation of the ECS Logo, then displays menu screen made menuPane into a field variable

--- src/main/java/uk/ac/soton/comp1206/scene/MenuScene.java
+++ src/main/java/uk/ac/soton/comp1206/scene/MenuScene.java
@@ -1,9 +1,12 @@
 package uk.ac.soton.comp1206.scene;
 
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import uk.ac.soton.comp1206.media.Multimedia;
@@ -16,7 +19,9 @@ import uk.ac.soton.comp1206.ui.GameWindow;
 public class MenuScene extends BaseScene {
 
     private static final Logger logger = LogManager.getLogger(MenuScene.class);
-
+    private ImageView logo;
+    private StackPane menuPane;
+    
     /**
      * Create a new menu scene
      * @param gameWindow the Game Window this will be displayed in
@@ -31,17 +36,14 @@ public class MenuScene extends BaseScene {
      */
     @Override
     public void build() {
-        Multimedia.playMusic("music/menu.mp3"); //background music
-        
         logger.info("Building " + this.getClass().getName());
 
         root = new GamePane(gameWindow.getWidth(),gameWindow.getHeight());
-
-        var menuPane = new StackPane();
+    
+        menuPane = new StackPane();
         menuPane.setMaxWidth(gameWindow.getWidth());
         menuPane.setMaxHeight(gameWindow.getHeight());
         menuPane.getStyleClass().add("menu-background");
-        root.getChildren().add(menuPane);
 
         var mainPane = new BorderPane();
         menuPane.getChildren().add(mainPane);
@@ -61,10 +63,44 @@ public class MenuScene extends BaseScene {
 
     /**
      * Initialise the menu
+     * Displays animated logo, then opens the menu screen
      */
     @Override
     public void initialise() {
-
+        Multimedia.playMusic("music/menu.mp3"); //background music
+        
+        //gets the logo image
+        var uri = getClass().getResource("/images/ECSGames.png").toExternalForm();
+        logo = new ImageView(uri);
+        logo.setFitWidth(gameWindow.getWidth());
+        logo.setOpacity(0);
+        
+        //creates a new temporary pane which displays the logo
+        var pane = new StackPane();
+        pane.setMaxWidth(gameWindow.getWidth());
+        pane.setMaxHeight(gameWindow.getHeight());
+        pane.getStyleClass().add("menu-black-fill");
+        
+        root.getChildren().add(pane);
+        pane.getChildren().add(logo);
+        
+        //fade the logo
+        fadeLogo();
+    }
+    
+    /**
+     * Fade Animation of the ECS Logo
+     * onFinished Property is set to display the menu
+     */
+    public void fadeLogo() {
+        var fade = new FadeTransition(Duration.millis(3000), logo);
+        fade.setFromValue(0.0);
+        fade.setToValue(1.0);
+        fade.play();
+        fade.onFinishedProperty().set((event) -> { //once fade finishes, display menu
+            root.getChildren().clear();
+            root.getChildren().add(menuPane);
+        });
     }
 
     /**
