2e2257c0375ef683ee7e61cd0f7a1b1fd0f5b4b1
bryanvullo <bryanvullo04@gmail.com>
2023-04-04 19:03:00 +0100

    New method to start the Scores Screen

--- src/main/java/uk/ac/soton/comp1206/ui/GameWindow.java
+++ src/main/java/uk/ac/soton/comp1206/ui/GameWindow.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import uk.ac.soton.comp1206.App;
+import uk.ac.soton.comp1206.game.Game;
 import uk.ac.soton.comp1206.network.Communicator;
 import uk.ac.soton.comp1206.scene.*;
 
@@ -98,6 +99,13 @@ public class GameWindow {
      * Display the single player challenge
      */
     public void startChallenge() { loadScene(new ChallengeScene(this)); }
+    
+    /**
+     * Display the scores screen
+     */
+    public void startScores(Game game) {
+        loadScene(new ScoresScene(this, game));
+    }
 
     /**
      * Setup the default settings for the stage itself (the window), such as the title and minimum width and height.
