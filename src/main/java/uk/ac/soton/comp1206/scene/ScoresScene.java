669fc785824536b9c731057f4eaa30bbb447bd2b
bryanvullo <bryanvullo04@gmail.com>
2023-04-08 14:34:58 +0100

    implemented online scores

--- src/main/java/uk/ac/soton/comp1206/scene/ScoresScene.java
+++ src/main/java/uk/ac/soton/comp1206/scene/ScoresScene.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
@@ -18,6 +19,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -26,6 +28,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import uk.ac.soton.comp1206.component.ScoreList;
 import uk.ac.soton.comp1206.game.Game;
+import uk.ac.soton.comp1206.network.Communicator;
 import uk.ac.soton.comp1206.ui.GamePane;
 import uk.ac.soton.comp1206.ui.GameWindow;
 
@@ -37,6 +40,7 @@ public class ScoresScene extends BaseScene {
     private static final Logger logger = LogManager.getLogger(ScoresScene.class);
     private Game game;
     private SimpleListProperty<Pair<String, Integer>> localScores;
+    private SimpleListProperty<Pair<String, Integer>> remoteScores;
     private String scoresFile = getClass().getResource("/scores.txt").getFile();
     private Scanner reader;
     private FileWriter writer;
@@ -44,6 +48,10 @@ public class ScoresScene extends BaseScene {
     private Comparator<Pair<String, Integer>> pairComparator = Comparator.comparingInt(Pair::getValue);
     private Comparator comparator = Collections.reverseOrder(pairComparator);
     private ScoreList scoreList;
+    private ScoreList remoteList;
+    private Communicator communicator = gameWindow.getCommunicator(); //TODO ask if this is okay
+    private final Object KEY = new Object();
+    private String name;
     
     /**
      * Creates a Scene Object
@@ -54,9 +62,16 @@ public class ScoresScene extends BaseScene {
         super(window);
         logger.info("Creating Scores Scene");
         this.game = game;
+        
+        //local scores
         var pairs = new ArrayList<Pair<String, Integer>>();
         var observablePairs = FXCollections.observableArrayList(pairs);
         localScores = new SimpleListProperty<>(observablePairs);
+        
+        //remote scores
+        var remotePairs = new ArrayList<Pair<String, Integer>>();
+        var observableRemotePairs = FXCollections.observableArrayList(remotePairs);
+        remoteScores = new SimpleListProperty<>(observableRemotePairs);
     }
     
     /**
@@ -67,18 +82,50 @@ public class ScoresScene extends BaseScene {
     @Override
     public void initialise() {
         scene.setOnKeyPressed(this::keyboardInput); //keyboard input to escape scene
-        
+        communicator.addListener(this::receiveCommunication);
+    
+        //load scores
         loadScores();
-        
+    
+        Boolean newScore = false;
         for (var pair : localScores) { //checks if game score has beaten any of the high scores
             var score = pair.getValue();
             if (game.score.get() > score) {
                 displayGetNameBox();
+                Platform.enterNestedEventLoop(KEY); //wait for name
+                updateScores(name);
+                newScore = true;
                 break;
             }
         }
-        scoreList.update(); //update the scores list
-        scoreList.reveal(); //animate
+    
+        requestRemoteScores();
+        //wait for remote scores to arrive
+        Platform.enterNestedEventLoop(KEY);
+        
+        for (var pair : remoteScores) { //checks if game score has beaten any of the online high scores
+            var score = pair.getValue();
+            if (game.score.get() > score) {
+                if (!newScore) { //check if we've already got the name
+                    displayGetNameBox();
+                    Platform.enterNestedEventLoop(KEY); //wait for name
+                }
+                sendHiScore(name, game.score.getValue().toString()); //send hiscore to server
+                //update the remote score list
+                remoteScores.remove(remoteScores.size()-1);
+                remoteScores.add(new Pair<>(name, game.score.getValue()));
+                remoteScores.sort(comparator);
+                
+                break;
+            }
+        }
+        //Update UI
+        scoreList.update();
+        remoteList.update();
+    
+        //reveal the scores
+        scoreList.reveal();
+        remoteList.reveal();
     }
     
     /**
@@ -113,12 +160,23 @@ public class ScoresScene extends BaseScene {
     
         var centerBox = new VBox();
         centerBox.setAlignment(Pos.CENTER);
+        mainPane.setCenter(centerBox);
+        
         var highScoreText = new Text("High Scores");
         highScoreText.getStyleClass().add("score-text");
-        mainPane.setCenter(centerBox);
+        centerBox.getChildren().add(highScoreText);
+        
+        var scoreBox = new HBox();
+        scoreBox.setAlignment(Pos.CENTER);
+        scoreBox.setSpacing(50);
+        centerBox.getChildren().add(scoreBox);
+        
         scoreList = new ScoreList("Local Scores");
         scoreList.pairs.bind(localScores);
-        centerBox.getChildren().addAll(highScoreText, scoreList);
+        
+        remoteList = new ScoreList("Online Scores");
+        remoteList.pairs.bind(remoteScores);
+        scoreBox.getChildren().addAll(scoreList, remoteList);
     }
     
     /**
@@ -142,13 +200,15 @@ public class ScoresScene extends BaseScene {
         //button action: set name and restore scores node at center
         button.setOnAction((event) -> {
             mainPane.setCenter(scores);
-            updateScores(field.getText());
+            name = field.getText();
+            Platform.exitNestedEventLoop(KEY, null);
         });
         
         field.setOnKeyPressed(event -> {
             if (event.getCode() == KeyCode.ENTER) {
                 mainPane.setCenter(scores);
-                updateScores(field.getText());
+                name = field.getText();
+                Platform.exitNestedEventLoop(KEY, null);
             }
         });
     }
@@ -165,14 +225,13 @@ public class ScoresScene extends BaseScene {
         } catch (FileNotFoundException e) {
             logger.debug("Local Scores file not found");
         }
-        int counter = 0;
-        while (reader.hasNextLine() & counter < 10) {
+        
+        while (reader.hasNextLine()) {
             var line = reader.nextLine();
             var pair = line.split(":");
             var user = pair[0];
             var score = Integer.parseInt(pair[1]);
-            localScores.add(counter, new Pair<>(user, score));
-            counter++;
+            localScores.add(new Pair<>(user, score));
         }
     }
     
@@ -253,8 +312,55 @@ public class ScoresScene extends BaseScene {
         localScores.sort(comparator); //sort the new list to put new pair in place
         
         writeScores(); //write new high scores to the file
+    }
+    
+    /**
+     * Send a request for the remote scores
+     */
+    private void requestRemoteScores() {
+        logger.info("Requesting Remote High Scores");
+        communicator.send("HISCORES UNIQUE");
+    }
+    
+    /**
+     * Parses the remote scores and adds them to the list property to display
+     * @param message the HISCORE message from the communicator
+     */
+    private void loadRemoteScores(String message) {
+        logger.info("Loading the Remote Scores");
+        var lines = message.split("\n");
+        for (int i = 0; i < 10; i++) {
+            var line = lines[i];
+            var pairs = line.split(":");
+            var name = pairs[0];
+            var score = Integer.parseInt(pairs[1]);
+            remoteScores.add(new Pair<>(name, score));
+        }
+        remoteList.update();
         
-        scoreList.update(); //update the scores list
-        scoreList.reveal(); //animate
+        Platform.exitNestedEventLoop(KEY, null); //reveal the scores
+    }
+    
+    /**
+     * This method parses the messages received from the communicator
+     * Calls the appropriate method to handle the message
+     * @param message received from the communicator
+     */
+    private void receiveCommunication(String message) {
+        logger.info("Received message");
+        var lines  = message.split(" ");
+        if (lines[0].equals("HISCORES")) {
+            Platform.runLater(() -> loadRemoteScores(lines[1]));
+        }
+    }
+    
+    /**
+     * Send a new HISCORE message to submit a new remote HISCORE
+     * @param user the name of the local player
+     * @param score the hiscore the player got
+     */
+    private void sendHiScore(String user, String score) {
+        logger.info("Submitting remote score {}:{}", user, score);
+        communicator.send("HISCORE " + user + ":" + score);
     }
 }
